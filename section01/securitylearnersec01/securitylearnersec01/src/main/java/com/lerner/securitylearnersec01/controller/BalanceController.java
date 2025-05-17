c10e801b38e5b52b0da383fa9d70bd4e13117079
Ravindra Shende <ravindrashende7867@gmail.com>
2025-05-17 18:37:52 +0530

    Security config class added

--- section01/securitylearnersec01/securitylearnersec01/src/main/java/com/lerner/securitylearnersec01/controller/BalanceController.java
+++ section01/securitylearnersec01/securitylearnersec01/src/main/java/com/lerner/securitylearnersec01/controller/BalanceController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BalanceController {
+	
     @GetMapping("/myBalance")
 	public String getBalanceDetails() {
 		return "Balance details";
