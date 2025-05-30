8555e6da98edb44f9a085aeafca56c4728188e9c
Ravindra Shende <ravindrashende7867@gmail.com>
2025-05-30 12:17:10 +0530

    new dependencies added to connect our web application with MySQL database

--- section01/securitylearnersec01/securitylearnersec01/src/main/java/com/lerner/securitylearnersec01/config/ProjectSecurityConfig.java
+++ section01/securitylearnersec01/securitylearnersec01/src/main/java/com/lerner/securitylearnersec01/config/ProjectSecurityConfig.java
@@ -4,8 +4,16 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -19,5 +27,39 @@ public class ProjectSecurityConfig {
 		http.httpBasic(withDefaults());
 		return http.build();
 	}
+	
+	@Bean
+	public UserDetailsService userDetailsService() {
+		
+//        UserDetails admin = User.withDefaultPasswordEncoder()
+//                .username("ravindra")
+//                .password("12345")
+//                .roles("ADMIN")
+//                .build();
+        
+        UserDetails admin = User.withUsername("ravindra")
+                .password("{noop}12345")
+                .authorities("ADMIN")
+                .build();
+        
+        UserDetails user = User.withUsername("alok")
+                .password("{bcrypt}$2a$12$pLKNTckp9hanlFqhdj7Q4uUQmB7sVp4exlIcDiOYEmFdKmX1xwHlS")
+                .authorities("read")
+                .build();
+        
+        
+        return new InMemoryUserDetailsManager(admin, user);
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();	
+	}
+	
+	@Bean
+	public CompromisedPasswordChecker compromisedPasswordChecker() {
+        return new HaveIBeenPwnedRestApiPasswordChecker();
+	}
+	
 
 }
